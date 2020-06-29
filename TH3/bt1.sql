f5ff7093d94d7195ddc8510839310649b326efd5
hoangtuyenblogger <hoangtuyenblogger@gmail.com>
2020-06-29 11:30:38 +0700

    Bài thưc hành 4

---
+++ TH3/bt1.sql
@@ -0,0 +1,128 @@
+﻿create database QL_BAN_HANG 
+go 
+use QL_BAN_HANG
+
+alter table HangHoa
+add foreign key(ma_loaihang) REFERENCES LoaiHang(ma_loaihang)
+
+alter table PhieuNhap
+add foreign key(ma_ncc) REFERENCES NhaCungCap(ma_ncc)
+alter table PhieuNhap
+add foreign key(ma_nv) REFERENCES NhanVien(ma_nv)
+
+
+alter table HoaDon
+add foreign key(ma_kh) REFERENCES KhachHang(ma_kh)
+alter table HoaDon
+add foreign key(ma_nv) REFERENCES NhanVien(ma_nv)
+
+
+alter table HoaDonCT
+add foreign key(ma_hh) references HangHoa(ma_hh)
+alter table HoaDonCT
+add foreign key(sohoadon) references HoaDon(sohoadon)
+
+
+
+-- ==================================================================================================================== --
+-- ==================================================================================================================== --
+
+-- 1. Cho xem danh sách nhân viên có tuổi từ 20 đến 40.
+SELECT        *
+FROM            NhanVien nv
+WHERE			(YEAR(GETDATE())-YEAR(nv.ngaysinh)) BETWEEN 20 AND 40
+
+
+
+
+-- ==================================================================================================================== --
+-- ==================================================================================================================== --
+/*-- 2. Cho xem danh sách nhân viên với độ tuổi. Thông tin kết quả bao gồm mã nhân viên, họ
+và tên, Phái, tuổi, độ tuổi. Trong đó Họ và tên là được ghép bởi HoNv và TenNv; Độ
+tuổi: dựa vào tuổi nếu tuổi <18: Thanh thiếu niên; Tuổi từ 18 đến < 35: Thanh niên; Tuổi
+từ 35 đến < 50: Trung niên; Tuổi >=50: Cao niên */
+
+
+SELECT ten_nv, ma_nv, (YEAR(GETDATE())-YEAR(nv.ngaysinh)) as tuoi, CASE WHEN (YEAR(GETDATE())-YEAR(nv.ngaysinh)) < 18 THEN 'Thanh thiếu niên' else 
+			case when (YEAR(GETDATE())-YEAR(nv.ngaysinh)) < 35 then 'Thanh niên' else
+			case when (YEAR(GETDATE())-YEAR(nv.ngaysinh)) < 50 then 'Trung niên' else
+			case when (YEAR(GETDATE())-YEAR(nv.ngaysinh)) >= 50 then 'Cao niên' end end end end dotuoi
+
+FROM            NhanVien nv
+
+-- ==================================================================================================================== --
+-- ==================================================================================================================== --
+/* Cho xem danh sách 3 nhân viên lớn tuổi nhất, thông tin gồm: MaNV, HoTen, GioiTinh,
+Tuoi */
+select top 3 nv.ma_nv, nv.ten_nv, (YEAR(GETDATE())-YEAR(nv.ngaysinh)) as 'Tuổi'
+from NhanVien nv
+order by (YEAR(GETDATE())-YEAR(nv.ngaysinh)) DESC
+
+-- ==================================================================================================================== --
+-- ==================================================================================================================== --
+/*
+4.
+Tạo query tính tiền trả trước của khách hàng, thông tin gồm: MaHD,
+MaKH,TenSP,DonGiaBan, Songay, Thanhtien, TienTraTruoc, Conlai. Trong đó:
+Songay = NgayGiaoHang – NgayLapHD; ThanhTien = SoLuong * DonGiaBan;
+TienTraTruoc: Nếu SoNgay <=20 thì trả trước 20% của thành tiền, nếu 20<SoNgay<=30
+thì trả trước 30% của thành tiền, các trường hợp còn lại trả trước 50% của thành tiền;
+Conlai=ThanhTien-TienTraTruoc
+*/
+select hehe.*, (hehe.thanh_tien - hehe.TienTraTruoc) as ConLai
+from
+(
+Select T.*, case when T.songay <= 20 then (0.2 * T.thanh_tien) else
+			case when T.songay between 20 and 30 then (0.3 * T.thanh_tien) else
+			(0.5 * T.thanh_tien) end end TienTraTruoc
+			 
+From
+						(SELECT        KhachHang.ma_kh, LoaiHang.ten_loaihang, HoaDonCT.dongia, dongia * soluong as thanh_tien , DATEDIFF(DAY,HoaDonCT.ngaygiaohang,HoaDon.ngayhoadon) as songay
+										
+						FROM            HoaDon INNER JOIN
+												 KhachHang ON HoaDon.ma_kh = KhachHang.ma_kh INNER JOIN
+												 HoaDonCT ON HoaDon.sohoadon = HoaDonCT.sohoadon CROSS JOIN
+												 LoaiHang
+												 ) as T
+) as hehe
+
+-- ==================================================================================================================== --
+-- ==================================================================================================================== --
+
+/*
+. 5.Tạo query cho xem tổng số lượng các sản phẩm đã bán. Thông tin bao gồm MaSP, TenSP,
+DonviTinh, TongSoLuong. Trong đó TongSoLuong là sum của Soluong, kết nhóm theo
+Masp, TenSP, DonviTinh
+*/
+SELECT        HangHoa.ma_hh, HangHoa.ten_hh, HangHoa.dv_tinh, SUM(soluong) as TongSoLuong
+FROM            HoaDonCT INNER JOIN
+                         HangHoa ON HoaDonCT.ma_hh = HangHoa.ma_hh
+GROUP BY  HangHoa.ma_hh, HangHoa.ten_hh, HangHoa.dv_tinh
+
+-- ==================================================================================================================== --
+-- ==================================================================================================================== --
+/*
+6. Tạo query tính tổng tiền của từng hóa đơn của từng khách hàng. Thông tin bao gồm
+MaKH, TenKH, MaHD, TongTienHD (Tổng tiền của từng hóa đơn). Trong đó:
+TongTienHD = Sum(SoLuong * DonGiaBan), kết nhóm theo MaKH, TenKH và MaHD.
+*/
+SELECT        KhachHang.ma_kh, KhachHang.ten_kh, HoaDonCT.sohoadon, SUM(soluong * dongia) as TongTienHoaDon
+FROM            HoaDonCT CROSS JOIN
+                         KhachHang
+GROUP BY KhachHang.ma_kh, KhachHang.ten_kh, HoaDonCT.sohoadon
+
+-- ==================================================================================================================== --
+-- ==================================================================================================================== --
+/*
+8. Tăng đơn giá của các sản phẩm lên 1%. 
+*/
+Update HoaDonCT SET dongia = dongia * 0.01
+-- ==================================================================================================================== --
+-- ==================================================================================================================== --
+/*
+9. Cập nhật đơn giá trong CTHoaDon bằng đơn giá trong HangHoa tăng thêm 20%.
+*/
+Update HoaDonCT 
+SET HDCT.dongia = 0.2 * HH.dongia
+FROM HoaDonCT HDCT, HangHoa HH
+WHERE HDCT.ma_hh = HDCT.ma_hh
---
+++ TH3/bt2.sql
@@ -0,0 +1,96 @@
+﻿/*
+1. Viết thủ tục xuất thông tin hóa đơn của nhân viên khi nhập mã nhân viên
+*/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Hoang Kim Tuyen
+-- Create date: 18/05/2020
+-- Description:	Viết thủ tục xuất thông tin hóa đơn của nhân viên khi nhập mã nhân viên
+-- exec xuat_thong_tin_hoa_don_nv 'nv1'
+-- =============================================
+alter PROCEDURE xuat_thong_tin_hoa_don_nv 
+	-- Add the parameters for the stored procedure here
+	@ms_nv nchar(50)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+SELECT        NhanVien.ma_nv, NhanVien.ten_nv, HoaDon.sohoadon, HoaDon.ngayhoadon, HoaDon.noidung, HoaDon.ma_kh
+FROM            HoaDon INNER JOIN
+                         NhanVien ON HoaDon.ma_nv = NhanVien.ma_nv
+WHERE @ms_nv = NhanVien.ma_nv
+END
+GO
+
+
+-- ==================================================================================================================== --
+-- ==================================================================================================================== --
+/*
+2. Viết thủ tục xuất thông tin chi tiết các hóa đơn của một khách hàng khi nhập mã khách
+hàng
+*/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Hoang Kim Tuyen
+-- Create date: 18/05/2020
+-- Description:	xuất thông tin chi tiết các hóa đơn
+-- exec : xuat_thong_tin_chi_tiet_hoa_don_khach_hang 'kh1'
+-- =============================================
+alter PROCEDURE xuat_thong_tin_chi_tiet_hoa_don_khach_hang 
+	-- Add the parameters for the stored procedure here
+	@ms_kh nchar(50)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+SELECT        KhachHang.ma_kh, KhachHang.ten_kh, HoaDonCT.sohoadonct, HoaDonCT.sohoadon, HoaDonCT.soluong, HoaDonCT.dongia, HoaDonCT.thanhtien
+FROM            HoaDonCT CROSS JOIN
+                         KhachHang
+WHERE @ms_kh =  KhachHang.ma_kh
+END
+GO
+
+-- ==================================================================================================================== --
+-- ==================================================================================================================== --
+/*
+3.Viết thủ tục xuất thông tin các hóa đơn được lập trong 1 khoảng thời gian do người dùng
+nhập.
+*/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Hoang Kim Tuyen
+-- Create date: 18/05/2020
+-- Description:	xuất thông tin chi tiết các hóa đơn
+-- exec : xuat_thong_tin_chi_tiet_hoa_don_khach_hang 'kh1'
+-- =============================================
+alter PROCEDURE xuat_thong_tin_chi_tiet_hoa_don_khach_hang 
+	-- Add the parameters for the stored procedure here
+	@ms_kh nchar(50)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+SELECT        KhachHang.ma_kh, KhachHang.ten_kh, HoaDonCT.sohoadonct, HoaDonCT.sohoadon, HoaDonCT.soluong, HoaDonCT.dongia, HoaDonCT.thanhtien
+FROM            HoaDonCT CROSS JOIN
+                         KhachHang
+WHERE @ms_kh =  KhachHang.ma_kh
+END
+GO
